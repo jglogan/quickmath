6036ae42df1eddaec5e01d03e8b571a1789bc6a5
J Logan <sgtbakerrahulnet@yahoo.com>
2021-12-15 19:16:49 -0800

    Add initial text to lifecycle. (#3)
    - Also change lifecycle time from 10 to 60 seconds.

--- src/components/TopicLifecycle.jsx
+++ src/components/TopicLifecycle.jsx
@@ -3,8 +3,9 @@ import {useLocation} from 'react-router-dom';
 
 import Box from 'Components/Box.jsx';
 
-const createFixedProblemCountLifecycle = (remainingCount, correctCount, incorrectCount) => {
+const createProblemCountLifecycle = (remainingCount, correctCount, incorrectCount, isInactive) => {
     return {
+        initialText: () => `Solve ${remainingCount + correctCount + incorrectCount} problems.`,
         activeText: () => `${remainingCount} problems left.`,
         completeText: () => `All done. You got ${correctCount} out of ${correctCount + incorrectCount} problems correct.`,
         handleAnswer: isCorrect => {
@@ -12,52 +13,59 @@ const createFixedProblemCountLifecycle = (remainingCount, correctCount, incorrec
             const updatedIncorrectCount = incorrectCount + (isCorrect ? 0 : 1);
             return createFixedProblemCountLifecycle(remainingCount - 1, updatedCorrectCount, updatedIncorrectCount);
         },
+        isActive: () => !isInactive,
         isComplete: () => remainingCount === 0,
     };
 };
 
-const createTimedLifecycle = (remainingMillis, correctCount, incorrectCount, onProgress) => {
+const createTimedLifecycle = (remainingMillis, correctCount, incorrectCount, onProgress, isInactive) => {
     const startMillis = Date.now();
     const boundedRemainingMillis = Math.max(0, remainingMillis);
-    const fractionalMillis = remainingMillis % 1000;
+    const fractionalMillis = boundedRemainingMillis % 1000;
     const durationMillis = fractionalMillis || 1000;
 
     let clearTimer = () => {};
-    if (boundedRemainingMillis > 0) {
+    if (!isInactive && boundedRemainingMillis > 0) {
         const timeoutId = setTimeout(() => {
-            onProgress(createTimedLifecycle(remainingMillis - durationMillis, correctCount, incorrectCount, onProgress));
+            onProgress(createTimedLifecycle(boundedRemainingMillis - durationMillis, correctCount, incorrectCount, onProgress));
         }, durationMillis);
         clearTimer = () => clearTimeout(timeoutId);
     }
 
+    const remainingSeconds = parseInt(boundedRemainingMillis / 1000);
     return {
         cleanup: clearTimer,
-        activeText: () => `${parseInt(remainingMillis / 1000)} seconds left.`,
+        initialText: () => `Solve as many problems as you can in ${remainingSeconds} seconds.`,
+        activeText: () => `${remainingSeconds} second${remainingSeconds === 1 ? '' : 's'} left.`,
         completeText: () => `All done. You got ${correctCount} out of ${correctCount + incorrectCount} problems correct.`,
         handleAnswer: isCorrect => {
             clearTimer();
             const elapsedMillis = Date.now() - startMillis;
             const updatedCorrectCount = correctCount + (isCorrect ? 1 : 0);
             const updatedIncorrectCount = incorrectCount + (isCorrect ? 0 : 1);
-            return createTimedLifecycle(remainingMillis - elapsedMillis, updatedCorrectCount, updatedIncorrectCount, onProgress);
+            return createTimedLifecycle(boundedRemainingMillis - elapsedMillis, updatedCorrectCount, updatedIncorrectCount, onProgress);
         },
-        isComplete: () => remainingMillis <= 0,
+        isActive: () => !isInactive,
+        isComplete: () => boundedRemainingMillis === 0,
     };
 };
 
 export const TopicLifecycle = props => {
     const problemCount = 10;
-    const durationSeconds = 10;
-    const createLifecycle = () => {
-        //return createFixedProblemCountLifecycle(problemCount, 0, 0);
-        return createTimedLifecycle(durationSeconds * 1000, 0, 0, setLifecycleState);
+    const durationSeconds = 60;
+    const createLifecycle = isInactive => {
+        //return createProblemCountLifecycle(problemCount, 0, 0, isInactive);
+        return createTimedLifecycle(durationSeconds * 1000, 0, 0, setLifecycleState, isInactive);
     };
 
     const renderLifecycleState = () => {
         if (!lifecycleState) {
+            return null;
+        }
+        else if (!lifecycleState.isActive()) {
             return (
                 <Box height='100%'>
-                    <p>Solve as many problems correctly as you can in {durationSeconds} seconds.</p>
+                    <p>{lifecycleState.initialText()}</p>
                     <button onClick={() => setLifecycleState(createLifecycle())}>Start</button>
                 </Box>
             );
@@ -90,17 +98,16 @@ export const TopicLifecycle = props => {
     };
 
     React.useEffect(() => {
-        cleanup();
+        return cleanup;
     }, []);
 
     const location = useLocation();
     React.useEffect(() => {
         cleanup();
-        setLifecycleState();
+        setLifecycleState(createLifecycle(true));
     }, [location]);
 
-    const [lifecycleState, setLifecycleState] = React.useState();
-
+    const [lifecycleState, setLifecycleState] = React.useState(createLifecycle(true));
     return renderLifecycleState();
 };
 
